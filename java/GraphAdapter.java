18c449563760c61195b9748ed3c15cfa3526feda
minio96 <mi.sypek96@gmail.com>
2020-01-12 20:04:36 +0100

    logging

--- java/GraphAdapter.java
+++ java/GraphAdapter.java
@@ -3,11 +3,16 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.alg.clique.DegeneracyBronKerboschCliqueFinder;
 import org.jgrapht.alg.shortestpath.BellmanFordShortestPath;
 import org.jgrapht.graph.DefaultEdge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraphAdapter {
+
+    protected static final Logger logger = LoggerFactory.getLogger(SdnLabTopologyListener.class);
+
     public void countShortestPathsAfterUpdate(String vertex, Graph<String, DefaultEdge> topology) {
         for (DefaultEdge outgoingEdge : topology.outgoingEdgesOf(vertex)){
-            BellmanFordShortestPath.findPathBetween(topology, vertex, topology.getEdgeTarget(outgoingEdge));
+            logger.debug(BellmanFordShortestPath.findPathBetween(topology, vertex, topology.getEdgeTarget(outgoingEdge)).toString());
         }
     }
 }
