9828ffa83276f3e0872d64b9cc7c687340e42370
mukasz <lukas.matuszy@gmail.com>
2020-01-15 12:48:30 +0100

    Add strict get mapping method

--- java/NodePortMap.java
+++ java/NodePortMap.java
@@ -4,14 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NodePortMap {
-	public final static Map<String, Map<String, String>> getMapping() {
+	public final static Map<String, Map<String, String>> getFullMap() {
 		Map<String, Map<String, String>> portMapping = new HashMap<>();
 		portMapping.put("S1", getS1Mapping());
 		portMapping.put("S2", getS2Mapping());
 		portMapping.put("S3", getS3Mapping());
 		portMapping.put("S4", getS4Mapping());
 		
-		
+		return portMapping;
+	}
+	
+	public final static String getMapping(String sourceNode, String destNode) {
+		return getFullMap().get(sourceNode).get(destNode);
 	}
 	
 	private static Map<String, String> getS1Mapping() {
